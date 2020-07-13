83218643e2bb45d80822dad571025da07da853b4
Zni <zarniaung19917@gmail.com>
2020-07-14 01:14:02 +0630

    Intial project.

---
+++ src/main/java/guru/springframework/sfgpetclinicgurututo/SfgPetClinicGuruTutoApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinicgurututo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicGuruTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicGuruTutoApplication.class, args);
+	}
+
+}
