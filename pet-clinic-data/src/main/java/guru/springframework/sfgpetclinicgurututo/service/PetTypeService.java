d56d37c879477bccf2215ad481a1e444fe96d7f8
Zni <zarniaung19917@gmail.com>
2020-07-27 00:42:11 +0630

    added PetTypeService.closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicgurututo/service/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinicgurututo.service;
+
+import guru.springframework.sfgpetclinicgurututo.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
