d81ce5861633f34a78f092dd11d87d8eda6b44bd
Zni <zarniaung19917@gmail.com>
2020-07-27 13:10:05 +0630

    added Specialties Map Impl.Closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicgurututo/service/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinicgurututo.service;
+
+import guru.springframework.sfgpetclinicgurututo.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
