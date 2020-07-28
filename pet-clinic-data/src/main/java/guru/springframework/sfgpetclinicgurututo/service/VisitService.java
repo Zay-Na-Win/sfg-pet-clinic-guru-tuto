e27444e1b17d749fd3ee714f31c01ad82795fe62
Zni <zarniaung19917@gmail.com>
2020-07-29 01:09:28 +0630

    Create visit map Service .Closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicgurututo/service/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinicgurututo.service;
+
+import guru.springframework.sfgpetclinicgurututo.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicgurututo/service/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,35 @@
+package guru.springframework.sfgpetclinicgurututo.service.springdatajpa;
+
+import guru.springframework.sfgpetclinicgurututo.model.Visit;
+import guru.springframework.sfgpetclinicgurututo.service.VisitService;
+import guru.springframework.sfgpetclinicgurututo.service.map.AbstractMapService;
+
+import java.util.Set;
+
+public class VisitSDJpaService implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return null;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Visit object) {
+
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
